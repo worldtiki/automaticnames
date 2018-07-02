5334f570a8bd0bd2db5104f48ffaeb31a91eaea6
Daniel Albuquerque <dmartinsalbuqu@hotels.com>
2018-07-02 13:15:57 +0100

    Initial commit

---
+++ src/main/java/com/intellij/java/modules/Example.java
@@ -0,0 +1,10 @@
+package com.intellij.java.modules;
+
+import zipkin2.Span;
+
+public class Example {
+
+    public void test(Span span) {
+        // do nothing
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,3 @@
+module haystack.proxy {
+    requires zipkin2;
+}
